9986bf60fbe4e8fa0cc06d7f38f472f84c17ed25
sarrthak008 <sarthaknavale08@gmail.com>
2024-08-09 05:14:58 +0530

    add paper postion

--- scripts/papers.js
+++ scripts/papers.js
@@ -1,5 +1,5 @@
 
-const Bcs =[{
+const Bcs ={
      sem1:[],
      sem2:[],
      sem3:[],
@@ -9,7 +9,7 @@ const Bcs =[{
      allSemister:[{
         url:""
     }]
-}]
+}
 
 const Bca =[{
     sem1:[],
@@ -23,3 +23,25 @@ const Bca =[{
    }]
 }]
 
+
+const loadFySem1 =()=>{
+    let sem1box = document.querySelector("#sem1")
+      let papers = Bcs.sem1
+       if(papers.length==0){
+           sem1box.innerHTML=`<h3>collecting data..</h3>`
+       }else{
+         
+           papers.forEach((paper,index)=>{
+              sem1box.innerHTML+=`<div class="iteam">
+                       <div class="iteam-img">
+                          <img src="../images/questionpaper.png" alt="">
+                       </div>
+                       <a href="${paper}" download class="downlaodBtn" target="_blank">view</a>
+                   </div>`
+           })
+
+       }
+        
+}
+
+loadFySem1()
