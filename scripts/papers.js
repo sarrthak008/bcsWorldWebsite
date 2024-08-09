30a5fc8ed6f9b9aede621b3a356c5dd28ed4902f
sarrthak008 <sarthaknavale08@gmail.com>
2024-08-09 05:40:18 +0530

    add bca and bcs papers links

--- scripts/papers.js
+++ scripts/papers.js
@@ -1,47 +1,85 @@
 
 const Bcs ={
-     sem1:[],
-     sem2:[],
-     sem3:[],
-     sem4:[],
-     sem5:[],
-     sem6:[],
-     allSemister:[{
-        url:""
-    }]
+     papers:[
+        {
+            url:"https://drive.google.com/file/d/1-Uf_VphA-ExrZ1Ub-gq63k-MaP4xOnWx/view?usp=drivesdk",
+            info:"FY to TY  all papers"
+        }
+
+        //add more papers like ul and info format
+
+     ]
 }
 
-const Bca =[{
-    sem1:[],
-    sem2:[],
-    sem3:[],
-    sem4:[],
-    sem5:[],
-    sem6:[],
-    allSemister:[{
-       url:""
-   }]
-}]
+const Bca ={
+    papers:[
+        {
+            url:"https://drive.google.com/file/d/1-mNjSAZspxR-Kxkgg1nf97w39zmCs5oj/view?usp=drivesdk",
+            info:"FY to TY all papers"
+        }
+    ]
+}
 
 
-const loadFySem1 =()=>{
-    let sem1box = document.querySelector("#sem1")
-      let papers = Bcs.sem1
+const LoadBcsPapers =()=>{
+
+    let sem1box = document.querySelector("#bcs")
+      let papers = Bcs.papers
        if(papers.length==0){
            sem1box.innerHTML=`<h3>collecting data..</h3>`
        }else{
          
            papers.forEach((paper,index)=>{
+            //console.log(paper)
               sem1box.innerHTML+=`<div class="iteam">
                        <div class="iteam-img">
-                          <img src="../images/questionpaper.png" alt="">
+                          <img src="../images/questionpaper.png" alt="" onclick="notify('${paper.info}')">
                        </div>
-                       <a href="${paper}" download class="downlaodBtn" target="_blank">view</a>
+                       <a href="${paper.url}" download class="downlaodBtn" target="_blank">view</a>
                    </div>`
            })
 
-       }
-        
+       }     
 }
 
-loadFySem1()
+
+
+const LoadBcaPapers =()=>{
+
+    let sem1box = document.querySelector("#bca")
+      let papers = Bca.papers
+     // console.log(papers)
+       if(papers.length==0){
+           sem1box.innerHTML=`<h3>collecting data..</h3>`
+       }else{
+         
+           papers.forEach((paper,index)=>{
+            //console.log(paper)
+              sem1box.innerHTML+=`<div class="iteam">
+                       <div class="iteam-img">
+                          <img src="../images/questionpaper.png" alt="" onclick="notify('${paper.info}')">
+                       </div>
+                       <a href="${paper.url}" download class="downlaodBtn" target="_blank">view</a>
+                   </div>`
+           })
+
+       }     
+}
+
+LoadBcsPapers()
+LoadBcaPapers()
+
+const notify =(meaage)=>{
+  notification(meaage,1500)
+}
+
+
+
+function notification(message, time) {
+    let notification = document.querySelector('.notification')
+    notification.style.display = "block"
+    notification.innerText = message
+    setTimeout(function () {
+        notification.style.display = "none"
+    }, time)
+}
